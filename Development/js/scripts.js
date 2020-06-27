a9534bf65a90604387ffed31fb790aca32d296d0
حسان العبدالجادر - 133 <hsnabduljader@gmail.com>
2020-06-27 20:31:35 +0300

    ...

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,29 +1,35 @@
 // السؤال الأول
-// 1
-
+// 1 
+const name = "حسان";
 // 2
-
+let counter = "14";
 // 3 and 4
-
+const student = { "حسان" "14"};
+console.log("انا اسمي" + " " + student[0] + student[1]);
 // 5 and 6
-
+console.log(counter*5);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+let hobbies ={ "كرة قدم, كرة سلة, بلايستيشن" };
 // 2
-
+hobbies.push(counter);
+console.log(hobbies);
 // 3
-
+hobbies.pop(counter);
+console.log(hobbies);
 // 4
-
+let student = { "web , css" };
 // 5
-
+student.push("طويل , رهف");
+console.log(student);
 // 6
-
+student.push("web");
+console.log(student);
 // 7
-
+student.push(رهف);
+console.log(student);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
